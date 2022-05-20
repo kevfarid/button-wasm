0f212f5edd359fbba5b5eb752419c7e0c3e7bfe3
Kevin Cruz <hi@kevfarid.dev>
2022-05-20 13:39:44 -0500

    feat(app): add sub button and styles

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,53 @@
+use wasm_bindgen::JsCast;
+use web_sys::{EventTarget, HtmlElement};
 use yew::prelude::*;
 
 struct Counter {
-    value: i32
+    value: i32,
 }
 
 #[function_component(App)]
 fn app() -> Html {
+    let state = use_state(|| Counter { value: 0 });
 
-    let state = use_state(|| Counter {
-        value: 0
-    });
-
-    let onclick = {
+    let edit_state = {
         let state = state.clone();
 
-        Callback::from(move |_| {
-            state.set(Counter {
-                value: state.value + 1
-            })
+        Callback::from(move |e: MouseEvent| {
+            let target: Option<EventTarget> = e.target();
+
+            let button = target.and_then(|t| t.dyn_into::<HtmlElement>().ok());
+
+            if let Some(button) = button {
+                let operation_type = button.get_attribute("id").unwrap_or("".to_string());
+
+                state.set(Counter {
+                    value: if operation_type == "sum" {
+                        state.value + 1
+                    } else {
+                        state.value - 1
+                    },
+                })
+            }
         })
     };
 
+    let onclick_sum = edit_state.clone();
+    let onclick_sub = edit_state.clone();
+
     html! {
-        <div>
-            <button {onclick}>{"+1"}</button>
-            <p> {state.value} </p>
-        </div>
+        <main>
+            <div class="button-group">
+                <button onclick={onclick_sum} id="sum">{"+1"}</button>
+                <button onclick={onclick_sub} id="sub">{"-1"}</button>
+            </div>
+            <div class="counter_wrapper">
+                <p> {state.value} </p>
+            </div>
+        </main>
     }
-    
 }
 
 fn main() {
-
     yew::start_app::<App>();
 }
